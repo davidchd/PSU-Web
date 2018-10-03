6b6756263e0a1e15d1cfd4c6c5959b57492cefe9
davidchd <davidchd@outlook.com>
2018-10-03 15:24:51 -0400

    Core features
    Implementing translation switches and left bar highlight

--- webpages/programming/programming.js
+++ webpages/programming/programming.js
@@ -1,17 +1,77 @@
 $(document).ready(function(){
 
-    $("#TranslateOn").css("text-decoration", "underline");
+    // declare variables
+    var translate = {
+        value: false,
+        change: function() {
+            $("#TranslateOn").css("text-decoration", (this.value ? "underline" : "none"));
+            $("#TranslateOff").css("text-decoration", (this.value ? "none" : "underline"));
+            $(".Chinese")[this.value ? "show" : "hide"]();
+        }
+    };
+    var $links = $(".UserLeftLink");
+    var selected = {
+        $cell: $("#wlcm"),
+        change: function() {
+            $links.css("background-color", "#ffffff");
+            $links.css("color", "#000000");
+            this.$cell.css("background-color", "#00ddff");
+            this.$cell.css("color", "#ffffff");
+        }
+    };
 
+    // init translation
+    translate.change();
+
+    // init highlight cell
+    selected.change();
+    $("#dynamic").load("subpages/Welcome.html");
+
+    // set translation on function
     $("#TranslateOn").click(function() {
-        $("#TranslateOn").css("text-decoration", "underline");
-        $("#TranslateOff").css("text-decoration", "none");
-        $(".Chinese").show();
+        translate.value = true;
+        translate.change();
     });
 
+    // set translation off function
     $("#TranslateOff").click(function() {
-        $("#TranslateOn").css("text-decoration", "none");
-        $("#TranslateOff").css("text-decoration", "underline");
-        $(".Chinese").hide();
+        translate.value = false;
+        translate.change();
+    });
+
+    // func for welcome
+    $("#wlcm").click(function() {
+        selected.$cell = $(this);
+        selected.change();
+        $("#dynamic").load("subpages/Welcome.html");
+    });
+
+    // func for introduction
+    $("#intro").click(function() {
+        selected.$cell = $(this);
+        selected.change();
+        $("#dynamic").load("subpages/Introduction.html");
+    });
+
+    // func for first program
+    $("#fst-prgm").click(function() {
+        selected.$cell = $(this);
+        selected.change();
+        $("#dynamic").load("subpages/FirstProgram.html");
+    });
+
+    // func for comments
+    $("#cmt").click(function() {
+        selected.$cell = $(this);
+        selected.change();
+        $("#dynamic").load("subpages/Comments.html");
+    });
+
+    // func for data types
+    $("#dt-tp").click(function() {
+        selected.$cell = $(this);
+        selected.change();
+        $("#dynamic").load("subpages/DataTypes.html");
     });
 
 });
