b3d6e229753c4875e875ccb8e66bfddcc1156b73
davidchd <davidchd@outlook.com>
2019-04-08 15:18:53 -0400

    Check for grammar errors v1
    before the research fair

--- webpages/programming/programming.js
+++ webpages/programming/programming.js
@@ -1,15 +1,20 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
     // declare variables
-    var $links = $(".UserLeftTitle,.UserLeftLink");
+    var $links = $(".UserLeftTitle,.UserLeftLink,.UserLeftBotLink");
     var agent = {
         value: navigator.userAgent,
+        ifPhone: function() {
+            return (this.value.indexOf("Android") + this.value.indexOf("iPhone") + this.value.indexOf("Windows Phone") + this.value.indexOf("SymbianOS") + this.value.indexOf("iPod") > 0);
+        },
+        isPhone: true,
         reloaded: function() {
-            if(this.value.indexOf("Android") + this.value.indexOf("iPhone") + this.value.indexOf("Windows Phone") + this.value.indexOf("SymbianOS") + this.value.indexOf("iPod") > 0) {
+            if(this.isPhone) {
                 $("body").css("font-size", "15px");
-                $("pre").each(function(i) {
+                $("pre").each(function() {
                     $(this).removeClass("line-numbers");
                     $(this).css("line-height", "1.2");
+                    $(this).after("<p class=\"ReloadedContent\"><small>Please access this page with a computer's browser for better experience as line numbers are disabled on the phone.</small></p>");
                 });
             } else {
                 $("body").css("font-size", "20px");
@@ -17,11 +22,18 @@ $(document).ready(function(){
         }
     };
     var selected = {
-        $cell: $("#wlcm"),
+        $cell: null,
+        load: function(href) {
+            $("#dynamic").load("subpages/basics/" + href, function() {
+                translate.view();
+                agent.reloaded();
+            });
+            this.change();
+        },
         change: function() {
             $links.css("background-color", "#ffffff");
             $links.css("color", "#000000");
-            this.$cell.css("background-color", "#00ddff");
+            this.$cell.css("background-color", "#00b7ff");
             this.$cell.css("color", "#ffffff");
         }
     };
@@ -31,27 +43,20 @@ $(document).ready(function(){
             $("#TranslateOn").css("text-decoration", (this.value ? "underline" : "none"));
             $("#TranslateOff").css("text-decoration", (this.value ? "none" : "underline"));
             $(".Chinese,.ReloadedChinese")[this.value ? "show" : "hide"]();
-            // agent.reloaded();
         }
     };
 
-    // set font size based on agent
-    agent.reloaded();
-    console.log(agent.value);
+    // initializing variables
+    agent.isPhone = agent.ifPhone();
+    console.log(agent.isPhone);
+    selected.$cell = $("#wlcm");
 
-    // init translation
-    translate.view();
-
-    // init highlight cell
-    selected.change();
-    $("#dynamic").load("subpages/basics/Welcome.html", function() {
-        translate.view();
-    });
-
-    // init code block
-    $("#BlockCode").each(function(){
-        $(this).html("<ol><li>" + $(this).html().replace(/\n/g,"\n</li><li>") +"\n</li></ol>");
-    });
+    // set left bar bottom
+    if(agent.isPhone) {
+        $("#botline").css("margin", (600 - $(".UserLeftTitle").length * 43 - $(".UserLeftLink").length * 28 - $(".UserLeftBotLink").length * 28 - $(".UserLeftBreak").length) + "px 0 0");
+    } else {
+        $("#botline").css("margin", (600 - $(".UserLeftTitle").length * 51 - $(".UserLeftLink").length * 34 - $(".UserLeftBotLink").length * 34 - $(".UserLeftBreak").length) + "px 0 0");
+    }
 
     // set translation on function
     $("#TranslateOn").click(function() {
@@ -65,54 +70,44 @@ $(document).ready(function(){
         translate.view();
     });
 
+    // init translation
+    // translate.view();
+
+    // init highlight cell
+    selected.load("Welcome.html");
+
     // func for welcome
     $("#wlcm").click(function() {
         selected.$cell = $(this);
-        selected.change();
-        $("#dynamic").load("subpages/basics/Welcome.html", function() {
-            translate.view();
-        });
+        selected.load("Welcome.html");
     });
 
     // func for introduction
     $("#intro").click(function() {
         selected.$cell = $(this);
-        selected.change();
-        $("#dynamic").load("subpages/basics/Introduction.html", function() {
-            translate.view();
-        });
+        selected.load("Introduction.html");
     });
 
     // func for first program
     $("#fst-prgm").click(function() {
         selected.$cell = $(this);
-        selected.change();
-        $("#dynamic").load("subpages/basics/FirstProgram.html", function() {
-            translate.view();
-        });
+        selected.load("FirstProgram.html");
     });
 
     // func for comments
     $("#cmt").click(function() {
         selected.$cell = $(this);
-        selected.change();
-        $("#dynamic").load("subpages/basics/Comments.html", function() {
-            translate.view();
-        });
+        selected.load("Comments.html");
     });
 
     // func for data types
     $("#dt-tp").click(function() {
         selected.$cell = $(this);
-        selected.change();
-        $("#dynamic").load("subpages/basics/DataTypes.html", function() {
-            translate.view();
-        });
+        selected.load("DataTypes.html");
     });
 
     $("#var").click(function() {
         selected.$cell = $(this);
-        selected.change();
         $("#dynamic").load("subpages/basics/Variables.html", function() {
             translate.view();
             var $CharDetail = $(".ReloadedCharDetail");
@@ -120,47 +115,49 @@ $(document).ready(function(){
             $("#ToggleCharDetail").click(function() {
                 $CharDetail.toggle("normal");
             });
+            agent.reloaded();
         });
+        selected.change();
     });
 
     $("#oprt").click(function() {
         selected.$cell = $(this);
-        selected.change();
-        $("#dynamic").load("subpages/basics/Operators.html", function() {
-            translate.view();
-        });
+        selected.load("Operators.html");
     });
 
     $("#func").click(function() {
         selected.$cell = $(this);
-        selected.change();
-        $("#dynamic").load("subpages/basics/Function.html", function() {
-            translate.view();
-        });
+        selected.load("Function.html");
     });
 
     $("#clg-func").click(function() {
         selected.$cell = $(this);
-        selected.change();
-        $("#dynamic").load("subpages/basics/CallingFunctions.html", function() {
-            translate.view();
-        });
+        selected.load("CallingFunctions.html");
     });
 
     $("#selec").click(function() {
         selected.$cell = $(this);
-        selected.change();
-        $("#dynamic").load("subpages/basics/Selection.html", function() {
-            translate.view();
-        });
+        selected.load("Selection.html");
     });
 
     $("#rpt").click(function() {
         selected.$cell = $(this);
-        selected.change();
-        $("#dynamic").load("subpages/basics/Repetition.html", function() {
-            translate.view();
-        });
+        selected.load("Repetition.html");
+    });
+
+    $("#ary").click(function() {
+        selected.$cell = $(this);
+        selected.load("Array.html");
+    });
+
+    $("#oprtlst").click(function() {
+        selected.$cell = $(this);
+        selected.load("OperatorList.html");
+    });
+
+    $("#rfak").click(function() {
+        selected.$cell = $(this);
+        selected.load("References.html");
     });
 
 });
